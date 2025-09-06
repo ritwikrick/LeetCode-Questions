8c7a85a321d041ce383a73af3368dcc3a744f9aa
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-09-07 02:34:44 +0530

    Time: 0 ms (100%), Space: 56.6 MB (15.1%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,11 +1,49 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-           if(arr[i]>arr[i+1]){
-                    return i;
-                }
+        //O(n)
+        // int n=arr.length;
+        // for(int i=0;i<n;i++){
+        //    if(arr[i]>arr[i+1]){
+        //             return i;
+        //         }
+        // }
+
+        // //O(logn)
+        // int n=arr.length;
+        // int left=0;
+        // int right=n-1;
+
+
+        // while(left<right){
+        //     int mid = left + (right - left) / 2;
+        //     if(arr[mid]<arr[mid+1]){
+        //         left=mid+1;
+        //     }else{
+        //         right=mid;
+        //     }
+        // }
+        // return left;
+        //O(n)
+        // int n=arr.length;
+        // int i;
+        // for( i=0;i<n;i++){
+        //     if(arr[i]>arr[i+1]){
+        //         break;
+        //     }
+        // }
+        // return i;
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
         }
-        return -1;
+        return low;
+
+
     }
 }
