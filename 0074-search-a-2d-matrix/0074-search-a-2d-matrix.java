a53708aa8e16b8663642d39dd3164db98570dc30
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-09-24 23:39:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (82.31%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix.length;j++){
+        //         if(matrix[i][j]==target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int left = 0, right = m * n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n]; // convert 1D index to 2D
+            
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+
+    }
+}
