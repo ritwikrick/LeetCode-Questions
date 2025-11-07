a68002dd5438e157a9e0bb881b7ae97401f57681
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-11-07 10:50:36 +0530

    Time: 5 ms (87.4%), Space: 45.4 MB (16.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,41 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // if(s.length()==0){
-        //     return 0;
-        // }
-        // int max=Integer.MIN_VALUE;
-        // for(int i=0;i<s.length();i++){
-        //     HashSet<Character> map=new HashSet<>();
-        //     int len=0;
-        //     for(int j=i;j<s.length();j++){
-        //         if(map.contains(s.charAt(j))){
-        //             break;
-        //         }
-        //         map.add(s.charAt(j));
-        //         len=j-i+1;
-        //     }
-        //     max=Math.max(max,len);
-
-        // }
-        // return max;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        int max = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-
-            if (map.containsKey(c)) {
-                // Move left only if needed
-                left = Math.max(left, map.get(c) + 1);
+        HashMap<Character,Integer> map=new HashMap<>();
+        int left=0;
+        int maxlen=0;
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            if(!map.containsKey(ch) || map.get(ch)<left){
+                map.put(ch,right);
+                maxlen=Math.max(maxlen,right-left+1);
+            }else {
+                left=map.get(ch)+1;
+                map.put(ch,right);
             }
-
-            map.put(c, right); // store latest index
-            max = Math.max(max, right - left + 1);
+            
         }
-
-        return max;
-
+        return maxlen;
     }
 }
