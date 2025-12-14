ad72c5cd15022183c786bbc46f790e10138ee291
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-12-14 10:06:27 +0530

    Time: 0 ms (100%), Space: 44.3 MB (20.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,28 +10,29 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       ListNode mergedll= new ListNode(-1);
-       ListNode temp=mergedll;
-       while(list1!=null && list2!=null){
-        if(list1.val<=list2.val){
+        ListNode ans=new ListNode(-1);
+        ListNode temp=ans;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }else{
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
+        }
+        while(list1!=null){
             temp.next=list1;
+            temp=temp.next;
             list1=list1.next;
-        }else{
+        }
+        while(list2!=null){
             temp.next=list2;
+            temp=temp.next;
             list2=list2.next;
         }
-        temp=temp.next;
-       }
-       while(list1 !=null){
-        temp.next=list1;
-        temp=temp.next;
-        list1=list1.next;
-       }
-       while(list2!=null){
-        temp.next=list2;
-        temp=temp.next;
-        list2=list2.next;
-       }
-       return mergedll.next;
+        return ans.next;
+
     }
 }
