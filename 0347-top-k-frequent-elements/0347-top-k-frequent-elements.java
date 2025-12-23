41656491c68a2dc2ba1efd7b619d5a8348bda534
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-12-23 11:11:26 +0530

    Time: 15 ms (48.21%), Space: 47.7 MB (50.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,14 +1,36 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // ArrayList<Integer> list=new ArrayList<>(map.keySet());
+        // Collections.sort(list, (a,b)->map.get(b)-map.get(a));
+        // //Collections.sort(list,(a,b)->a-b);
+        // int ans[]=new int[k];
+        // for(int i=0;i<k;i++){
+        //     ans[i]=list.get(i);
+        // }
+        // return ans;
+
+        //Efficient-> Min Heap(O(mlogk))
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
         ArrayList<Integer> list=new ArrayList<>(map.keySet());
-        Collections.sort(list, (a,b)->map.get(b)-map.get(a));
+        for(int i=0;i<list.size();i++){
+            int key=list.get(i);
+            int val=map.get(key);
+            pq.add(new int[]{key,val});
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
         int ans[]=new int[k];
         for(int i=0;i<k;i++){
-            ans[i]=list.get(i);
+            ans[i]=pq.remove()[0];
         }
         return ans;
        
