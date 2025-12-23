b4baced66f2fb2ed38411628ea10e4744893f494
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-12-23 10:35:39 +0530

    Time: 20 ms (31.67%), Space: 51 MB (50.55%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,22 +1,47 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int m=matrix[0].length;
-        int n=matrix.length;
+        // int m=matrix[0].length;
+        // int n=matrix.length;
         
-        ArrayList<Integer> list=new ArrayList<>();
+        // ArrayList<Integer> list=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         list.add(matrix[i][j]);
+        //     }
+        // }
+        // Collections.sort(list,(a,b)->a-b);
+        // int c=0;
+        // for(int i=0;i<list.size();i++){
+        //     if(i==k-1){
+        //         int num=list.get(i);
+        //         return num;
+        //     }
+        // }
+        // return -1;
+
+        //Efficient approach
+
+        int n=matrix.length;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                list.add(matrix[i][j]);
+            pq.add(new int[]{matrix[i][0],i,0});
+        }
+
+        int count=0;
+        int ans=0;
+        while(!pq.isEmpty()){
+            int[] curr=pq.remove();
+            ans=curr[0];
+            int r=curr[1];
+            int c=curr[2];
+            count++;
+
+            if(count==k) return ans;
+
+            if(c+1<n){
+                pq.add(new int[]{matrix[r][c+1],r,c+1});
             }
         }
-        Collections.sort(list,(a,b)->a-b);
-        int c=0;
-        for(int i=0;i<list.size();i++){
-            if(i==k-1){
-                int num=list.get(i);
-                return num;
-            }
-        }
-        return -1;
+        return ans;
     }
 }
