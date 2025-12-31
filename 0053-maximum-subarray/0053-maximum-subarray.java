8c61fe0f7a2313136ab445e44da29154229e1ceb
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-12-31 16:14:34 +0530

    Time: 1 ms (99.79%), Space: 77 MB (90.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -12,12 +12,11 @@ class Solution {
         int maxsum=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
-             maxsum=Math.max(sum,maxsum);
+            sum+=nums[i];
+            maxsum=Math.max(maxsum,sum);
             if(sum<0){
                 sum=0;
-            } 
-           
+            }
         }
         return maxsum;
     }
