df3119be341669b3ffec78ce4268cdc3e22e5712
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2025-12-31 15:49:36 +0530

    Time: 5 ms (45.29%), Space: 69.8 MB (22.05%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,32 +1,113 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-    //     double max=Integer.MIN_VALUE;
-    //     for(int i=0;i<nums.length-k+1;i++){
-    //         double sum=0;
-    //         for(int j=i;j<i+k;j++){
-    //             sum=sum+nums[j];
-    //             max=Math.max(max,sum);
-    //         }
-    //     }
-    //     return max/k;
-    // }
-    int n=nums.length;
-    double sum=0;
-    for(int i=0;i<k;i++){
-        sum=sum+nums[i];
-    }
-    double max=sum;
-    int l=0;
-    int r=k;
-    while(r<n){
-        sum+=nums[r] - nums[l];
-        l++;
-        r++;
-        max=Math.max(sum,max);
-    }
-    return max/k;
-    }
+        // double avg=0;
+        // double maxavg=-Double.MAX_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     double sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         int c=j-i+1;
+        //         if(c==k){
+        //             avg=sum/c;
+        //             maxavg=Math.max(maxavg,avg);
+        //         }
+
+        //     }
+        // }
+        // return maxavg;
+        double sum=0;
+        //Find sum upto first window
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        double maxsum=sum;
+        int left=0;
+        for(int right=k;right<nums.length;right++){
+            sum=sum-nums[left];
+            left++;
+            sum=sum+nums[right];
+            maxsum=Math.max(maxsum,sum);
+        }
+        return maxsum/k;
 
 
-
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public double findMaxAverage(int[] nums, int k) {
+//     //     double max=Integer.MIN_VALUE;
+//     //     for(int i=0;i<nums.length-k+1;i++){
+//     //         double sum=0;
+//     //         for(int j=i;j<i+k;j++){
+//     //             sum=sum+nums[j];
+//     //             max=Math.max(max,sum);
+//     //         }
+//     //     }
+//     //     return max/k;
+//     // }
+//     int n=nums.length;
+//     double sum=0;
+//     for(int i=0;i<k;i++){
+//         sum=sum+nums[i];
+//     }
+//     double max=sum;
+//     int l=0;
+//     int r=k;
+//     while(r<n){
+//         sum+=nums[r] - nums[l];
+//         l++;
+//         r++;
+//         max=Math.max(sum,max);
+//     }
+//     return max/k;
+//     }
+
+
+
+// }
