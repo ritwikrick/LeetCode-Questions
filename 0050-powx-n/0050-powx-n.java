749df17c1147d2af92e738d7cfb9ec5dc6446a98
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-03 12:09:37 +0530

    Time: 0 ms (100%), Space: 48.3 MB (10.29%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;
+        long N = n;          // convert to long to avoid overflow
         if (N < 0) {
             x = 1 / x;
             N = -N;
         }
+        return power(x, N);
+    }
 
-        double result = 1;
-        while (N > 0) {
-            if ((N & 1) == 1) { // same as N % 2 == 1, but uses bitwise
-                result = result * x;
-            }
-            x = x * x;
-            N = N / 2;
+    private double power(double x, long n) {
+        if (n == 0) return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
         }
-        return result;
     }
 }
