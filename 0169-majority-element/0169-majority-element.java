f1b3d1f62a5d0f1630197dbf8655191c57698251
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-05 09:56:50 +0530

    Time: 1 ms (99.82%), Space: 55.6 MB (58.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,74 +1,31 @@
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         for(int i=0;i<nums.length;i++){
-//             int c=0;
-//            for(int j=i+1;j<nums.length;j++){
-//                 if(nums[i]==nums[j]){
-//                     c++;
-//                     if(c>=nums.length/2){
-//                     return nums[i];
-//                     }
-//                 }
-                
-//             }
-//         }
-//         return -1;
-        
-        
-//     }
-// }
-
-// class Solution {
-//     //Rick
-//     public int majorityElement(int[] nums) {
-//         int candidate = nums[0], count = 1;
-
-//         for (int i = 1; i < nums.length; i++) {
-//             if (nums[i] == candidate) {
-//                 count++;  // Increase count if same as candidate
-//             } else {
-//                 count--;  // Decrease count if different
-//                 if (count == 0) {  // If count reaches 0, change candidate
-//                     candidate = nums[i];
-//                     count = 1;
-//                 }
-//             }
-//         }
-//         return candidate;
-//     }
-// }
-
 class Solution {
     public int majorityElement(int[] nums) {
-        // Step 1: Find a candidate using Boyer-Moore Majority Vote Algorithm
-        int candidate = nums[0], count = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == candidate) {
+        // int ans=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int c=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             c++;
+        //         }
+        //     }
+        //      if(c>nums.length/2){
+        //             ans=nums[i];
+        //         }
+        // }
+        // return ans;
+        int candidate=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==candidate){
                 count++;
-            } else {
+            }else{
                 count--;
-                if (count == 0) {
-                    candidate = nums[i];
-                    count = 1;
+                if(count==0){
+                    candidate=nums[i];
+                    count=1;
                 }
             }
         }
-
-        // Step 2: Validate the candidate (only needed if majority is not guaranteed)
-        int frequency = 0;
-        for (int num : nums) {
-            if (num == candidate) {
-                frequency++;
-            }
-        }
-
-        // Check if it appears more than n/2 times
-        if (frequency > nums.length / 2) {
-            return candidate;
-        } else {
-            return -1; // No majority element found
-        }
+        return candidate;
     }
 }
-
