b933807bd2ac13308bebdf07aa8ec6059ee322a0
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-07 11:58:04 +0530

    Time: 1 ms (99.79%), Space: 51.3 MB (25.96%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -11,18 +11,33 @@ class Solution {
         //     }
         // }
         // return c;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
-        int psum=0;
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            psum+=nums[i];
-            if(map.containsKey(psum-goal)){
-                c=c+map.get(psum-goal);
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // map.put(0,1);
+        // int psum=0;
+        // int c=0;
+        // for(int i=0;i<nums.length;i++){
+        //     psum+=nums[i];
+        //     if(map.containsKey(psum-goal)){
+        //         c=c+map.get(psum-goal);
+        //     }
+        //     map.put(psum,map.getOrDefault(psum,0)+1);
+        // }
+        // return c;
+        return countt(nums,goal)- countt(nums,goal-1);
+    }
+    public int countt(int[] nums,int k){
+        if(k<0) return 0;
+        int left=0, count=0,sum=0;
+        for(int right=0;right<nums.length;right++){
+            sum+=nums[right];
+            while(sum>k){
+                sum-=nums[left];
+                left++;
             }
-            map.put(psum,map.getOrDefault(psum,0)+1);
+            if(sum<=k){
+                count+=right-left+1;
+            }
         }
-        return c;
-        
+        return count;
     }
 }
