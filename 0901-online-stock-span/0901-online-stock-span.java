22d43c0d880a067dbe40437517ae1fa505b32668
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-15 21:03:19 +0530

    Time: 28 ms (87.51%), Space: 54.7 MB (20.92%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,23 @@
+class StockSpanner {
+
+    // Stack stores {price, span}
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+
+        // Pop while previous price <= current price
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        // Push current price and its span
+        stack.push(new int[]{price, span});
+
+        return span;
+    }
+}
