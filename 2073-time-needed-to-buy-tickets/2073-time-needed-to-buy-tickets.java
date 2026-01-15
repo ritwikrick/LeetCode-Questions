b5aa64a4cab88f77e640db4402169d369ee4d78d
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-15 23:16:56 +0530

    Time: 11 ms (15.8%), Space: 46.1 MB (17.5%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,18 +1,32 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+        //Storing index at queue and modifying values in tickets array.
         Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<tickets.length;i++){
             q.add(i);
         }
+        // while(!q.isEmpty()){
+        //     int idx=q.remove();
+        //     tickets[idx]--;
+        //     c++;
+        //     if (tickets[idx] == 0 && idx == k) {
+        //         return c;
+        //     }
+        //     if(tickets[idx]>0){
+        //         q.add(idx);
+        //     }
+        // }
+        // return c;
         int c=0;
         while(!q.isEmpty()){
             int curr=q.remove();
             tickets[curr]--;
             c++;
-            if (tickets[curr] == 0 && curr == k) {
+            if(tickets[curr]==0 && curr==k) {
+                q.remove(curr);
                 return c;
             }
-            if(tickets[curr]>0){
+            if(tickets[curr]!=0){
                 q.add(curr);
             }
         }
