5d80301fbf78e82dcbe05486b31afca14bc2e49a
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-17 23:32:37 +0530

    Time: 8 ms (16.34%), Space: 45.2 MB (70.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,71 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ds=new ArrayList<>();
         Arrays.sort(candidates);
-        find(0,candidates,target,ds,ans);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        csum(ans,list,candidates,target,0,candidates.length);
         return ans;
     }
-    private void find(int ind,int arr[],int target,List<Integer> ds,List<List<Integer>> ans){
+    public void csum(List<List<Integer>> ans,List<Integer> list,int nums[],int target,int i,int n){
+
         if(target==0){
-            ans.add(new ArrayList<>(ds));
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=ind;i<arr.length;i++){
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(arr[i]> target) break;
-            ds.add(arr[i]);
-            find(i+1,arr,target-arr[i],ds,ans);
-            ds.remove(ds.size()-1);
+        if (target < 0 || i == nums.length) {
+            return;
         }
+        //add
+        list.add(nums[i]);
+        csum(ans,list,nums,target-nums[i],i+1,n);
+
+        //skip duplicate
+        int next = i + 1;
+        while (next < nums.length && nums[next] == nums[i]) {
+            next++;
+        }
+
+        //skip
+        list.remove(list.size()-1);
+        csum(ans,list,nums,target,next,n);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//         Arrays.sort(candidates);
+//         List<List<Integer>> ans=new ArrayList<>();
+//         List<Integer> list=new ArrayList<>();
+//         HashSet<ArrayList<Integer>> set=new HashSet<>();
+//         csum(list,set,candidates,target,0,candidates.length);
+//         return new ArrayList<>(set);
+//     }
+//     public void csum(List<Integer> list,HashSet<ArrayList<Integer>> set,int nums[],int target,int i,int n){
+//         if(target==0){
+//             set.add(new ArrayList<>(list));
+//         }
+//         if(target<0 || i==n){
+//             return;
+//         }
+//         //add
+//         list.add(nums[i]);
+//         csum(list,set,nums,target-nums[i],i+1,n);
+//         //skip
+//         list.remove(list.size()-1);
+//         csum(list,set,nums,target,i+1,n);
+//     }
+// }
