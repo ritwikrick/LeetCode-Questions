e69a8fcaced86f8e084874e9bf5966742e9f09d0
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-20 12:28:15 +0530

    Time: 8 ms (38.97%), Space: 45.4 MB (23.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,37 +1,24 @@
 class Solution {
     public String reverseWords(String s) {
-        // Trim to remove leading/trailing spaces
-        s = s.trim();
-        int n = s.length();
-        StringBuilder ans = new StringBuilder();
-
-        // Reverse the entire string
-        StringBuilder reversed = new StringBuilder(s).reverse();
-
-        int i = 0;
-        while (i < n) {
-            // If it's a space, skip it
-            if (reversed.charAt(i) == ' ') {
-                i++;
-                continue;
+        s=s.trim();
+        StringBuilder ans=new StringBuilder();
+        StringBuilder sb=new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            char c=s.charAt(i);
+            if(c!=' '){
+                sb.append(c);
+            }else{
+                ans.append(sb.reverse());
+                if(sb.length()!=0){
+                    ans.append(' ');
+                }
+                sb=new StringBuilder();
+                
             }
-
-            StringBuilder word = new StringBuilder();
-
-            // Build word until we hit a space or end
-            while (i < n && reversed.charAt(i) != ' ') {
-                word.append(reversed.charAt(i));
-                i++;
-            }
-
-            // Reverse the current word to correct its order
-            word.reverse();
-
-            // Append the word to the final answer with space
-            ans.append(word).append(" ");
         }
-
-        // Remove the last extra space
+        if(sb.length()>0){
+            ans.append(sb.reverse());
+        }
         return ans.toString().trim();
     }
 }
