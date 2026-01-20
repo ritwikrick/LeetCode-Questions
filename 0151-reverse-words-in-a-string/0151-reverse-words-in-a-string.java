f4510b9b162ddb4fe9954bfa2bdef336b8a13a9c
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-20 12:46:02 +0530

    Time: 7 ms (65.23%), Space: 44.4 MB (53.66%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,25 +1,41 @@
 class Solution {
     public String reverseWords(String s) {
-        s=s.trim();
-        StringBuilder ans=new StringBuilder();
-        StringBuilder sb=new StringBuilder();
-        for(int i=s.length()-1;i>=0;i--){
-            char c=s.charAt(i);
-            if(c!=' '){
-                sb.append(c);
-            }else{
-                ans.append(sb.reverse());
-                if(sb.length()!=0){
-                    ans.append(' ');
-                }
-                sb=new StringBuilder();
+        //O(n) but using extra space
+        // s=s.trim();
+        // StringBuilder ans=new StringBuilder();
+        // StringBuilder sb=new StringBuilder();
+        // for(int i=s.length()-1;i>=0;i--){
+        //     char c=s.charAt(i);
+        //     if(c!=' '){
+        //         sb.append(c);
+        //     }else{
+        //         ans.append(sb.reverse());
+        //         if(sb.length()!=0){
+        //             ans.append(' ');
+        //         }
+        //         sb=new StringBuilder();
                 
+        //     }
+        // }
+        // // if(sb.length()>0){
+        // //     ans.append(sb.reverse());
+        // // }
+        // ans.append(sb.reverse());
+        // return ans.toString().trim();
+
+
+
+        String words[]=s.split("\\s+");
+        //It safely removes multiple spaces, tabs, or newlines between words.
+        StringBuilder sb=new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]);
+            if(i!=0){
+                sb.append(' ');
             }
         }
-        // if(sb.length()>0){
-        //     ans.append(sb.reverse());
-        // }
-        ans.append(sb.reverse());
-        return ans.toString().trim();
+        return sb.toString().trim();
+
+        
     }
 }
