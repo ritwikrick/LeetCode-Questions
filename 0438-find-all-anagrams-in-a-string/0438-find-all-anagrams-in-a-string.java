1978e4e9a4f90eee99899447c1f08724909176b1
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-01-29 21:22:14 +0530

    Time: 749 ms (9.03%), Space: 47.7 MB (20.48%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,43 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        if (s.length() < p.length()) return res;
-
-        HashMap<Character, Integer> pMap = new HashMap<>();
-        HashMap<Character, Integer> sMap = new HashMap<>();
-
-        // Build p frequency map
-        for (char c : p.toCharArray()) {
-            pMap.put(c, pMap.getOrDefault(c, 0) + 1);
-        }
-
-        int window = p.length();
-
-        // First window
-        for (int i = 0; i < window; i++) {
-            char c = s.charAt(i);
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
-        }
-
-        if (sMap.equals(pMap)) res.add(0);
-
-        // Slide window
-        for (int i = window; i < s.length(); i++) {
-            char add = s.charAt(i);
-            char remove = s.charAt(i - window);
-
-            // add new char
-            sMap.put(add, sMap.getOrDefault(add, 0) + 1);
-
-            // remove old char
-            sMap.put(remove, sMap.get(remove) - 1);
-            if (sMap.get(remove) == 0) sMap.remove(remove);
-
-            if (sMap.equals(pMap)) {
-                res.add(i - window + 1);
+        int n=p.length();
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<=s.length()-n;i++){
+            String subs=s.substring(i,i+n);
+            if(anagram(subs,p)){
+                list.add(i);
             }
         }
-
-        return res;
+        return list;
+    }
+    public boolean anagram(String s, String p){
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<p.length();i++){
+            freq[p.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
