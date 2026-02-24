880cdc113c914df509606a6368e0899dfe688239
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-02-25 00:06:53 +0530

    Time: 5 ms (81.31%), Space: 77.5 MB (48.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,21 +1,44 @@
 class Solution {
     public int maxArea(int[] height) {
+        // int maxw=Integer.MIN_VALUE;
+        // int left=0;
+        // int right=height.length-1;
+        // while(left!=right){
+        //     int ht=Math.min(height[left],height[right]);
+        //     int width=right-left;
+        //     maxw=Math.max(maxw,ht*width);
+
+        //     if(height[left]<height[right]){
+        //         left++;
+        //     }else{
+        //         right--;
+        //     }
+            
+        // }
+        // return maxw;
+
         int maxw=Integer.MIN_VALUE;
         int left=0;
         int right=height.length-1;
-        while(left!=right){
+        while(left<right){
             int ht=Math.min(height[left],height[right]);
-            int width=right-left;
-            maxw=Math.max(maxw,ht*width);
-
-            if(height[left]<height[right]){
-                left++;
-            }else{
+            int water=ht*(right-left);
+            maxw=Math.max(water,maxw);
+            if(height[left]>height[right]){
                 right--;
+            }else{
+                left++;
             }
-            
         }
         return maxw;
+
+
+
+
+
+
+
+
 
     }
 }
