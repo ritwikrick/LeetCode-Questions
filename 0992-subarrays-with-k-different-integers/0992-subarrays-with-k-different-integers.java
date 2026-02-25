b0b47c1cfecb499bcb3d253d7eadfef205680bec
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-02-25 11:18:25 +0530

    Time: 49 ms (59.84%), Space: 50.6 MB (5.02%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -3,18 +3,35 @@ class Solution {
         return exact(nums,k)-exact(nums,k-1);
     }
     public int exact(int nums[],int k){
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int left=0;
+        // int count=0;
+        // for(int right=0;right<nums.length;right++){
+        //     map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+        //     while(map.size()>k){
+        //         map.put(nums[left],map.get(nums[left])-1);
+        //         if(map.get(nums[left])==0) map.remove(nums[left]);
+        //         left++;
+        //     }
+        //     if(map.size()<=k) count+=right-left+1;
+        // }
+        // return count;
         HashMap<Integer,Integer> map=new HashMap<>();
-        int left=0;
-        int count=0;
-        for(int right=0;right<nums.length;right++){
-            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+        int l=0;
+        int c=0;
+        for(int r=0;r<nums.length;r++){
+            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
             while(map.size()>k){
-                map.put(nums[left],map.get(nums[left])-1);
-                if(map.get(nums[left])==0) map.remove(nums[left]);
-                left++;
+                map.put(nums[l],map.get(nums[l])-1);
+                if(map.get(nums[l])==0){
+                    map.remove(nums[l]);
+                }
+                l++;
             }
-            if(map.size()<=k) count+=right-left+1;
+            if(map.size()<=k){
+                c+=r-l+1;
+            }
         }
-        return count;
+        return c;
     }
 }
