4321822f4aba3eb41ef59d377a5882246a45f180
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-03-27 12:02:31 +0530

    Time: 7 ms (86.86%), Space: 46.6 MB (61.1%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,3 +1,43 @@
+//MAX-HEAP
+// class Pair implements Comparable<Pair>{
+//     String word;
+//     int freq;
+//     Pair(String word, int freq){
+//         this.word=word;
+//         this.freq=freq;
+//     }
+//     @Override
+//     public int compareTo(Pair p){
+//         //Max heap
+//         if(p.freq!=this.freq){
+//             return p.freq-this.freq;
+//         }
+//         //if freq is same, then we will sort acc to lexicographically order.
+//         return this.word.compareTo(p.word);
+//     }
+// }
+// class Solution {
+//     public List<String> topKFrequent(String[] words, int k) {
+//         PriorityQueue<Pair> pq=new PriorityQueue<>();
+//         HashMap<String, Integer> map=new HashMap<>();
+//         for(int i=0;i<words.length;i++){
+//             map.put(words[i],map.getOrDefault(words[i],0)+1);
+//         }
+//         ArrayList<String> list=new ArrayList<>(map.keySet());
+//         for(int i=0;i<list.size();i++){
+//             String key=list.get(i);
+//             int val=map.get(key);
+//             pq.add(new Pair(key,val));
+//         }
+//         ArrayList<String> ans=new ArrayList<>();
+//         for(int i=0;i<k;i++){
+//             ans.add(pq.remove().word);
+//         }
+//         return ans;
+//     }
+// }
+//-----------------------------------------------------------------------------------
+//MIN-HEAP
 class Pair implements Comparable<Pair>{
     String word;
     int freq;
@@ -7,12 +47,12 @@ class Pair implements Comparable<Pair>{
     }
     @Override
     public int compareTo(Pair p){
-        //Max heap
+        //Min heap
         if(p.freq!=this.freq){
-            return p.freq-this.freq;
+            return this.freq-p.freq;
         }
         //if freq is same, then we will sort acc to lexicographically order.
-        return this.word.compareTo(p.word);
+        return p.word.compareTo(this.word);
     }
 }
 class Solution {
@@ -27,11 +67,15 @@ class Solution {
             String key=list.get(i);
             int val=map.get(key);
             pq.add(new Pair(key,val));
+            if(pq.size()>k){
+                pq.remove();
+            }
         }
         ArrayList<String> ans=new ArrayList<>();
         for(int i=0;i<k;i++){
             ans.add(pq.remove().word);
         }
+        Collections.reverse(ans);
         return ans;
     }
 }
