b2d5dd30461134ca7a2cb00c3f97285df6fb1a12
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-03-27 11:29:37 +0530

    Time: 13 ms (79.59%), Space: 47.7 MB (58.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,3 +1,18 @@
+class Number implements Comparable<Number>{
+    int ele;
+    int freq;
+    Number(int ele, int freq){
+        this.ele=ele;
+        this.freq=freq;
+    }
+    @Override
+    public int compareTo(Number n){
+        //max heap
+        // return n.freq-this.freq;
+        //min-heap
+        return this.freq-n.freq;
+    }
+}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         // HashMap<Integer,Integer> map=new HashMap<>();
@@ -14,23 +29,62 @@ class Solution {
         // return ans;
 
         //Efficient-> Min Heap(O(mlogk))
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        // ArrayList<Integer> list=new ArrayList<>(map.keySet());
+        // for(int i=0;i<list.size();i++){
+        //     int key=list.get(i);
+        //     int val=map.get(key);
+        //     pq.add(new int[]{key,val});
+        //     if(pq.size()>k){
+        //         pq.remove();
+        //     }
+        // }
+        // int ans[]=new int[k];
+        // for(int i=0;i<k;i++){
+        //     ans[i]=pq.remove()[0];
+        // }
+        // return ans;
+
+        //MAX-Heap O(m log m)
+        // PriorityQueue<Number> pq=new PriorityQueue<>();
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // ArrayList<Integer> list=new ArrayList<>(map.keySet());
+        // for(int i=0;i<list.size();i++){
+        //     int key=list.get(i);
+        //     int val=map.get(key);
+        //     pq.add(new Number(key,val));
+        // }
+        // int ans[]=new int[k];
+        // for(int i=0;i<k;i++){
+        //     ans[i]=pq.remove().ele;
+        // }
+        // return ans;
+       //---------------------------------------------------------------------
+       //MIN-HEAP O(m log k)
+        PriorityQueue<Number> pq=new PriorityQueue<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
         ArrayList<Integer> list=new ArrayList<>(map.keySet());
         for(int i=0;i<list.size();i++){
             int key=list.get(i);
             int val=map.get(key);
-            pq.add(new int[]{key,val});
+            pq.add(new Number(key,val));
             if(pq.size()>k){
                 pq.remove();
             }
         }
         int ans[]=new int[k];
         for(int i=0;i<k;i++){
-            ans[i]=pq.remove()[0];
+            ans[i]=pq.remove().ele;
         }
         return ans;
        
