c09b337104d1a540a49814fd65a1f0fd6228633c
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-05-17 09:21:43 +0530

    Time: 0 ms (100%), Space: 45.1 MB (22.99%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -11,21 +11,36 @@ class Solution {
     //     return ans;
     // }
 
-     public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return recc(dp,cost, n);
-    }
-    public int recc(int dp[], int cost[], int n){
-        if(n<=1) return 0;
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n=cost.length;
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return recc(dp,cost, n);
+    // }
+    // public int recc(int dp[], int cost[], int n){
+    //     if(n<=1) return 0;
 
-        if(dp[n]!=-1){
-            return dp[n];
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int prev1= cost[n-1]+ recc(dp,cost,n-1);
+    //     int prev2= cost[n-2]+ recc(dp,cost,n-2);
+    //     dp[n]=Math.min(prev1, prev2);
+    //     return dp[n];
+    // }
+
+    //-------------------------
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        if(n<=1) return 0;
+        int dp[]=new int [n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=cost.length;i++){
+            int one=cost[i-1]+ dp[i-1];
+            int two= cost[i-2]+ dp[i-2];
+            dp[i]=Math.min(one,two);
         }
-        int prev1= cost[n-1]+ recc(dp,cost,n-1);
-        int prev2= cost[n-2]+ recc(dp,cost,n-2);
-        dp[n]=Math.min(prev1, prev2);
         return dp[n];
     }
 }
