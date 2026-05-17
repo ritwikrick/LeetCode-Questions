30fdceec3882b128fe997d4e6fac1ed6089acc1f
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-05-17 08:14:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,36 @@
 class Solution {
-    public int climbStairs(int n) {
-        //fibonacci variant- using recursion
-        // if(n==0 || n==1) return 1;
-        // return climbStairs(n-1) + climbStairs(n-2);
-        //////////////////////////////////////////////////////////////
+    // public int climbStairs(int n) {
+    //     //fibonacci variant- using recursion
+    //     // if(n==0 || n==1) return 1;
+    //     // return climbStairs(n-1) + climbStairs(n-2);
+    //     //////////////////////////////////////////////////////////////
 
-        //Memoization
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(dp,n);
+    //     //Memoization
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return solve(dp,n);
+    // }
+    // public int solve(int dp[], int n){
+    //     if(n<=2) return n;
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     dp[n]=solve(dp,n-1)+solve(dp,n-2);
+    //     return dp[n];
+
+    // }
+
+    //-------------------------------------
+    //TABULATION
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+       int dp[]=new int[n+1];
+       dp[1]=1;
+       dp[2]=2;
+       for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
-    public int solve(int dp[], int n){
-        if(n==1){
-            return 1;
-        }
-        if(n==2) return 2;
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]=solve(dp,n-1) + solve(dp,n-2);
-        return dp[n];
-    }
+    
 }
