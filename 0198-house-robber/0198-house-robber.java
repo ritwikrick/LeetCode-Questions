172e3b43bf03c4b73b56f6875a81847758f302ea
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-05-19 16:02:51 +0530

    Time: 0 ms (100%), Space: 42.6 MB (86.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -16,25 +16,65 @@ class Solution {
     //     return ans;
     // }
 
+    // public int rob(int[] nums) {
+    //     int n=nums.length;
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return recc(dp,nums,n-1);
+    // }
+    // public int recc(int dp[], int nums[], int idx){
+    //     if(idx==0){
+    //         return nums[0];
+    //     }
+    //     if(idx==-1){
+    //         return 0;
+    //     }
+    //     if(dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
+    //     int pick= nums[idx]+ recc(dp,nums,idx-2);
+    //     int nopick=recc(dp,nums,idx-1);
+    //     dp[idx]= Math.max(pick, nopick);
+    //     return dp[idx];
+    // }
+    //recursion
+    // public int rob(int[] nums) {
+    //     int n=nums.length;
+    //     return recc(nums, n-1);
+    // }
+    // public int recc(int nums[], int n){
+    //     //base case
+    //     if(n<0){
+    //         return 0;
+    //     }
+
+    //     //pick
+    //     int pick=nums[n]+recc(nums,n-2);
+    //     int skip=recc(nums,n-1);
+    //     return Math.max(pick,skip);
+    // }
+
+    //memoization
     public int rob(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n+1];
+        int dp[]=new int[n];
         Arrays.fill(dp,-1);
-        return recc(dp,nums,n-1);
+        return recc(dp,nums, n-1);
     }
-    public int recc(int dp[], int nums[], int idx){
-        if(idx==0){
-            return nums[0];
-        }
-        if(idx==-1){
+    public int recc(int dp[],int nums[], int n){
+        //base case
+        if(n<0){
             return 0;
         }
-        if(dp[idx]!=-1){
-            return dp[idx];
+        //Storing values in dp array
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        int pick= nums[idx]+ recc(dp,nums,idx-2);
-        int nopick=recc(dp,nums,idx-1);
-        dp[idx]= Math.max(pick, nopick);
-        return dp[idx];
+
+        //pick
+        int pick=nums[n]+recc(dp,nums,n-2);
+        int skip=recc(dp,nums,n-1);
+        dp[n]= Math.max(pick,skip);
+        return dp[n];
     }
 }
