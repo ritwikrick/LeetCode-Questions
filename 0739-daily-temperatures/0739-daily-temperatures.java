3e09ef5f9385c57dad200863f8df178212b2343d
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-07-19 08:31:47 +0530

    Time: 76 ms (34.8%), Space: 106.7 MB (67.35%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,15 +1,31 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int n=temperatures.length;
         Stack<Integer> s=new Stack<>();
-        int ans[]=new int [n];
-        for(int i=0;i<n;i++){
-            while(!s.isEmpty() && temperatures[i]>temperatures[s.peek()]){
-                int prevIndex=s.pop();
-                ans[prevIndex]=i-prevIndex;
+        int ans[]= new int[temperatures.length];
+        for(int i=temperatures.length-1;i>=0;i--){
+            while(!s.isEmpty() && temperatures[s.peek()]<=temperatures[i]){
+                s.pop();
+            }
+            if(!s.isEmpty()){
+                ans[i]=s.peek()-i;
             }
             s.push(i);
         }
         return ans;
+
+
+        //O(N^2)
+        // int arr[]=new int[temperatures.length];
+        // for(int i=0;i<temperatures.length;i++){
+        //     int curr=temperatures[i];
+        //     for(int j=i;j<temperatures.length;j++){
+        //         if(curr<temperatures[j]){
+        //             int c=j-i;
+        //             arr[i]=c;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return arr;
     }
 }
