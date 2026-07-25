d266563490586848251b1e8fbad45715ae61001f
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-07-25 11:06:36 +0530

    Time: 54 ms (66.97%), Space: 70.3 MB (75.01%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,43 +1,24 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-
-        // HashMap<Integer,Integer> map=new HashMap<>();
-        // int n=fruits.length;
-        // if(n==0) return 0;
-        // int maxlen=0;
-        // int left=0;
-        // for(int right=0;right<n;right++){
-        //     map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-        //     if(map.size()>2){
-        //         int l=fruits[left];
-        //         left++;
-        //         map.put(l,map.get(l)-1);
-        //         if(map.get(l)==0) map.remove(l);
-        //     }
-        //     if(map.size()<=2){
-        //         maxlen= Math.max(maxlen,right-left+1);
-        //     }
-        // }
-        // return maxlen;
+        HashMap<Integer, Integer> map=new HashMap<>();
+        int res=0;
         int left=0;
-        int maxlen=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
         for(int right=0;right<fruits.length;right++){
+            //operations with right
             map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-
-            if(map.size()>2){
+            //if condition breaks
+            while(map.size()>2){
                 map.put(fruits[left],map.get(fruits[left])-1);
-                if(map.get(fruits[left])==0) map.remove(fruits[left]);
-                //Because a fruit type with count 0 is no longer in the window, and
-//              👉 map.size() must reflect the number of distinct fruits in the         current           window.//
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
                 left++;
             }
+            // if it is in valid window
             if(map.size()<=2){
-                maxlen=Math.max(maxlen,right-left+1);
+                res=Math.max(res, right-left+1);
             }
-
         }
-        return maxlen;
-
+        return res;
     }
 }
