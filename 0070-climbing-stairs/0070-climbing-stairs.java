e8ff147f7edd8a9c8adfaee5931bb807e0ef405c
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-07-27 08:29:37 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,56 +1,39 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         return recc(n); 
+//     }
+//     public int recc(int n){
+//         if(n==0 || n==1){
+//             return 1;
+//         }
+//         if(n<0){
+//             return 0;
+//         }
+//         return recc(n-1)+recc(n-2);
+//     }
+// }
+
 class Solution {
-    // public int climbStairs(int n) {
-    //     //fibonacci variant- using recursion
-    //     // if(n==0 || n==1) return 1;
-    //     // return climbStairs(n-1) + climbStairs(n-2);
-    //     //////////////////////////////////////////////////////////////
-
-    //     //Memoization
-    //      tc- O(n)
-            //sc- O(n)+O(n)[array+ recursion stack]
-    //     int dp[]=new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return solve(dp,n);
-    // }
-    // public int solve(int dp[], int n){
-    //     if(n<=2) return n;
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-    //     dp[n]=solve(dp,n-1)+solve(dp,n-2);
-    //     return dp[n];
-
-    // }
-
-    //-------------------------------------
-    // //TABULATION
-    // public int climbStairs(int n) {
-    //     //TC- O(N)
-    //     //SC-O(N)[ARRAY]
-    //     if(n<=2) return n;
-    //    int dp[]=new int[n+1];
-    //    dp[1]=1;
-    //    dp[2]=2;
-    //    for(int i=3;i<=n;i++){
-    //         dp[i]=dp[i-1]+dp[i-2];
-    //    }
-    //    return dp[n];
-    // }
-
-    //TABULATION
     public int climbStairs(int n) {
-        //TC- O(N)
-        //SC-O(1)
-        if(n<=2) return n;
-        int prev1=2;
-        int prev2=1;
-        int ans=0;
-       for(int i=3;i<=n;i++){
-            ans=prev1+prev2;
-            prev2=prev1;
-            prev1=ans;
-       }
-       return ans;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return recc(n,dp);
     }
-    
+    public int recc(int n, int dp[]){
+        //Step 1: Initialization
+        if(n==0){
+            //we have 1 way- ie.to not move
+            return 1;
+        }
+        if(n<0){
+            //we cant go under land
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=recc(n-1,dp)+ recc(n-2,dp);
+        return dp[n];
+
+    }
 }
