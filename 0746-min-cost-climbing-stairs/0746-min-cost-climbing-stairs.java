af3539e7a1ca4f97988976bb3b043d7079fa937a
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-07-27 09:21:41 +0530

    Time: 1 ms (39.38%), Space: 45 MB (40.94%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,46 +1,41 @@
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n=cost.length;
+//          return Math.min(
+//                 recc(cost, n - 1),
+//                 recc(cost, n - 2)
+//         );//because we wanted to reach the top. suppose arr len is 3(0,1,2), we can reach top(at 3) by min(2 or 1)
+//     }
+//     public int recc(int[] cost, int n){//this only finds to reach the last index(2) with min cost
+//         //initialization
+//         if(n==0) return cost[0];
+//         if(n==1) return cost[1];
+//         if(n==-1) return 0;
+//         //pick
+//         int onestep=cost[n]+recc(cost,n-2);
+//         int twostep=cost[n]+recc(cost,n-1);
+//         return Math.min(onestep,twostep);
+
+//     }
+// }
+//MEMOIZATION
 class Solution {
-    // public int minCostClimbingStairs(int[] cost) {
-    //     int n=cost.length;
-    //     return recc(cost, n);
-    // }
-    // public int recc(int cost[], int n){
-    //     if(n<=1) return 0;
-    //     int prev1= cost[n-1]+ recc(cost,n-1);
-    //     int prev2= cost[n-2]+ recc(cost,n-2);
-    //     int ans=Math.min(prev1, prev2);
-    //     return ans;
-    // }
-
-    // public int minCostClimbingStairs(int[] cost) {
-    //     int n=cost.length;
-    //     int dp[]=new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return recc(dp,cost, n);
-    // }
-    // public int recc(int dp[], int cost[], int n){
-    //     if(n<=1) return 0;
-
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-    //     int prev1= cost[n-1]+ recc(dp,cost,n-1);
-    //     int prev2= cost[n-2]+ recc(dp,cost,n-2);
-    //     dp[n]=Math.min(prev1, prev2);
-    //     return dp[n];
-    // }
-
-    //-------------------------
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
-        if(n<=1) return 0;
         int dp[]=new int [n+1];
-        dp[0]=0;
-        dp[1]=0;
-        for(int i=2;i<=cost.length;i++){
-            int one=cost[i-1]+ dp[i-1];
-            int two= cost[i-2]+ dp[i-2];
-            dp[i]=Math.min(one,two);
-        }
+        Arrays.fill(dp,-1);
+        return Math.min((recc(dp,cost,n-1)), (recc(dp,cost,n-2)));
+    }
+    public int recc(int dp[], int cost[], int n){
+        //initialization
+        if(n==0 || n==1) return cost[n];
+        //check
+        if(dp[n]!=-1) return dp[n];
+        //one step
+        int onestep=cost[n]+recc(dp,cost,n-1);
+        int twostep=cost[n]+recc(dp,cost,n-2);
+        dp[n]= Math.min(onestep, twostep);
         return dp[n];
+
     }
 }
