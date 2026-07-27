550de0dc5d3375648147fbb5b2868ee1c3374fda
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-07-27 08:32:58 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -13,27 +13,47 @@
 //     }
 // }
 
+//MEMOIZATION
+// class Solution {
+//     public int climbStairs(int n) {
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return recc(n,dp);
+//     }
+//     public int recc(int n, int dp[]){
+//         //Step 1: Initialization
+//         if(n==0){
+//             //we have 1 way- ie.to not move
+//             return 1;
+//         }
+//         if(n<0){
+//             //we cant go under land
+//             return 0;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         dp[n]=recc(n-1,dp)+ recc(n-2,dp);
+//         return dp[n];
+
+//     }
+// }
+
+//TABULATION
 class Solution {
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return recc(n,dp);
-    }
-    public int recc(int n, int dp[]){
-        //Step 1: Initialization
         if(n==0){
-            //we have 1 way- ie.to not move
             return 1;
         }
         if(n<0){
-            //we cant go under land
             return 0;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        dp[n]=recc(n-1,dp)+ recc(n-2,dp);
         return dp[n];
-
     }
 }
