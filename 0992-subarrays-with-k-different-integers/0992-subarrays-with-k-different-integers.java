aeafd22c18b9553e8351f148d743b714bd59d2b0
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-10 07:57:57 +0530

    Time: 51 ms (27.12%), Space: 50.4 MB (27.01%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,37 +1,25 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return exact(nums,k)-exact(nums,k-1);
+        return atmost(nums,k) - atmost(nums,k-1);
     }
-    public int exact(int nums[],int k){
-        // HashMap<Integer,Integer> map=new HashMap<>();
-        // int left=0;
-        // int count=0;
-        // for(int right=0;right<nums.length;right++){
-        //     map.put(nums[right],map.getOrDefault(nums[right],0)+1);
-        //     while(map.size()>k){
-        //         map.put(nums[left],map.get(nums[left])-1);
-        //         if(map.get(nums[left])==0) map.remove(nums[left]);
-        //         left++;
-        //     }
-        //     if(map.size()<=k) count+=right-left+1;
-        // }
-        // return count;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int l=0;
-        int c=0;
-        for(int r=0;r<nums.length;r++){
-            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+    public int atmost(int nums[], int k){
+        HashMap<Integer, Integer> map=new HashMap<>();
+        int left=0;
+        int count=0;
+        for(int right=0;right<nums.length;right++){
+            int val=nums[right];
+            map.put(val,map.getOrDefault(val,0)+1);
             while(map.size()>k){
-                map.put(nums[l],map.get(nums[l])-1);
-                if(map.get(nums[l])==0){
-                    map.remove(nums[l]);
-                }
-                l++;
+                //dec freq of left
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0) map.remove(nums[left]);
+                //incrementing left
+                left++;
             }
             if(map.size()<=k){
-                c+=r-l+1;
+                count+=right-left+1;
             }
         }
-        return c;
+        return count;
     }
 }
