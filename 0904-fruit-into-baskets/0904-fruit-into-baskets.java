ad797482c5f651d1acda69b015f2d558ea3ed6ff
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-10 08:13:00 +0530

    Time: 56 ms (29.09%), Space: 70.6 MB (29.38%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,24 +1,24 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> map=new HashMap<>();
-        int res=0;
-        int left=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int left=0,count=0;
         for(int right=0;right<fruits.length;right++){
-            //operations with right
             map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-            //if condition breaks
             while(map.size()>2){
                 map.put(fruits[left],map.get(fruits[left])-1);
+                //check if nums[left] is equal to zero or not
                 if(map.get(fruits[left])==0){
+                    //remove it
                     map.remove(fruits[left]);
                 }
+                //move forward left
                 left++;
             }
-            // if it is in valid window
+            //if->its a valid window ->then
             if(map.size()<=2){
-                res=Math.max(res, right-left+1);
+                count=Math.max(count,right-left+1);
             }
         }
-        return res;
+        return count;
     }
 }
