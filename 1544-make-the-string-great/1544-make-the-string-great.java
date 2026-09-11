47750afe18566d608dc13b0edad5ca350cbc0b5c
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-11 13:36:02 +0530

    Time: 3 ms (64.09%), Space: 43.9 MB (34.16%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String makeGood(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (st.isEmpty()) {
+                st.push(ch);
+            }
+            else if (Math.abs(st.peek() - ch) == 32) {
+                st.pop();
+            }
+            else {
+                st.push(ch);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
+        }
+
+        return ans.reverse().toString();
+    }
+}
