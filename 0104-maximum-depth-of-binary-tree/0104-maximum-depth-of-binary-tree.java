fb21bd3fe559cc750b49fbd56c7a5f991017ddc6
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-13 09:01:59 +0530

    Time: 0 ms (100%), Space: 47 MB (45.94%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,8 +18,8 @@ class Solution {
         if(root==null){
             return 0;
         }
-        int lh=maxDepth(root.left);
-        int rh=maxDepth(root.right);
-        return Math.max(lh,rh)+1;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
     }
 }
