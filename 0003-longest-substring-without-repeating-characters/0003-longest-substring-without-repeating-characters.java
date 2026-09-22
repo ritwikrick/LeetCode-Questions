63f21330b2165f9dd1566fd40a4023860ced4c46
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-23 00:04:14 +0530

    Time: 45 ms (62.91%), Space: 48.1 MB (11.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,55 +1,20 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         HashMap<Character,Integer> map=new HashMap<>();
-//         int left=0;
-//         int maxlen=0;
-//         for(int right=0;right<s.length();right++){
-//             char ch=s.charAt(right);
-//             if(!map.containsKey(ch) || map.get(ch)<left){
-//                 map.put(ch,right);
-//                 maxlen=Math.max(maxlen,right-left+1);
-//             }else {
-//                 left=map.get(ch)+1;
-//                 map.put(ch,right);
-//             }
-            
-//         }
-//         return maxlen;
-//     }
-// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashMap<Character,Integer> map=new HashMap<>();
-        // int maxlen=0;
-        // int left=0;
-        // for(int right=0;right<s.length();right++){
-        //     char ch=s.charAt(right);
-        //     if(!map.containsKey(ch)  || map.get(ch)<left){
-        //         map.put(ch,right);
-        //         maxlen=Math.max(maxlen,right-left+1);
-        //     }else{
-        //         left=map.get(ch)+1;
-        //         map.put(ch,right);
-        //     }
-        // }
-        // return maxlen;
-
         HashMap<Character,Integer> map=new HashMap<>();
         int left=0;
+        int len=0;
         int maxlen=0;
-        for(int r=0;r<s.length();r++){
-            char ch=s.charAt(r);
-            if(!map.containsKey(ch) || map.get(ch)<left){
-                map.put(ch,r);
-                maxlen=Math.max(maxlen,r-left+1);
+        for(int right=0;right<s.length();right++){
+            char c=s.charAt(right);
+            if(!map.containsKey(c) || map.get(c)<left){
+                map.put(c,right);
+                len=right-left+1;
+                maxlen=Math.max(maxlen,len);
             }else{
-                left=map.get(ch)+1;
-                map.put(ch,r);
+                left=map.get(c)+1;
+                map.put(c,right);
             }
         }
         return maxlen;
-
-
-
     }
 }
