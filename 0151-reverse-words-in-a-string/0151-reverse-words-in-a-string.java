ea78f3512a8b4120e5b3f42d5ef84a76dac1c6f3
Rick Ritwik <129302876+ritwikrick@users.noreply.github.com>
2026-09-23 00:53:34 +0530

    Time: 11 ms (10.87%), Space: 47.1 MB (5.49%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,41 +1,16 @@
 class Solution {
     public String reverseWords(String s) {
-        //O(n) but using extra space
-        // s=s.trim();
-        // StringBuilder ans=new StringBuilder();
-        // StringBuilder sb=new StringBuilder();
-        // for(int i=s.length()-1;i>=0;i--){
-        //     char c=s.charAt(i);
-        //     if(c!=' '){
-        //         sb.append(c);
-        //     }else{
-        //         ans.append(sb.reverse());
-        //         if(sb.length()!=0){
-        //             ans.append(' ');
-        //         }
-        //         sb=new StringBuilder();
-                
-        //     }
-        // }
-        // // if(sb.length()>0){
-        // //     ans.append(sb.reverse());
-        // // }
-        // ans.append(sb.reverse());
-        // return ans.toString().trim();
-
-
-
-        String words[]=s.split("\\s+");
-        //It safely removes multiple spaces, tabs, or newlines between words.
-        StringBuilder sb=new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            sb.append(words[i]);
-            if(i!=0){
-                sb.append(' ');
-            }
+        String arr[] = s.trim().split("\\s+");
+        String narr[]=new String[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--){
+            narr[j]=arr[i];
+            j++;
         }
-        return sb.toString().trim();
-
-        
+        String ans="";
+        for(int i=0;i<narr.length;i++){
+            ans+=narr[i] + " ";
+        }
+        return ans.trim();
     }
 }
